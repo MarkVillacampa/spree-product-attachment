f4dcb3a3871a69ded7e157a49a04f68c2ac0cfef
Mark Villacampa <markvjal@gmail.com>
2012-09-30 17:23:31 +0200

    .

--- app/overrides/add_admin_tab_decorator.rb
+++ app/overrides/add_admin_tab_decorator.rb
@@ -1,4 +1,4 @@
-Deface::Override.new(:virtual_path => "spree/admin/shared/product_tabs",
+Deface::Override.new(:virtual_path => "spree/admin/shared/_product_tabs",
                      :name => "attachments_download_tab",
                      :insert_after => "[data-hook='admin_product_tabs'], #admin_product_tabs[data-hook]",
                      :partial => "admin/shared/product_downloadables_tab",
