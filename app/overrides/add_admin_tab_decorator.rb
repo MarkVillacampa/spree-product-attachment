9a61d1c7bf72cd3e16c50b05dd2610e62e95594b
Mark Villacampa <markvjal@gmail.com>
2012-09-30 17:08:55 +0200

    changed position of the download tabs override

--- app/overrides/add_admin_tab_decorator.rb
+++ app/overrides/add_admin_tab_decorator.rb
@@ -1,4 +1,4 @@
-Deface::Override.new(:virtual_path => "spree/admin/shared/product_tabs",
+Deface::Override.new(:virtual_path => "spree/admin/products/edit",
                      :name => "print_buttons",
                      :insert_after => "[data-hook='admin_product_tabs']",
                      :partial => "admin/shared/product_downloadables_tab",
