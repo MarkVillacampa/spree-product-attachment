0f6e509a065b73e77d95b61bf5df4daf303000b7
Mark Villacampa <markvjal@gmail.com>
2012-09-30 19:19:26 +0200

    .

--- app/controllers/spree/admin/downloadables_controller.rb
+++ app/controllers/spree/admin/downloadables_controller.rb
@@ -1,90 +1,89 @@
 module Spree
-  module Admin
-    class DownloadablesController < ResourceController
-      before_filter :load_data
+  class Admin::DownloadablesController < Admin::ResourceController
+    before_filter :load_data
 
-      create.before :set_viewable
-      update.before :set_viewable
+    create.before :set_viewable
+    update.before :set_viewable
 
-      # new_action.after do
-      #   respond_to do |wants|
-      #     wants.html {render :action => :new, :layout => false}
-      #   end
-      # end
+    # new_action.after do
+    #   respond_to do |wants|
+    #     wants.html {render :action => :new, :layout => false}
+    #   end
+    # end
 
-      # create.after do
-      #   respond_to do |wants|
-      #     wants.html {redirect_to admin_product_downloadables_url(@product)}
-      #   end
-      # end
+    # create.after do
+    #   respond_to do |wants|
+    #     wants.html {redirect_to admin_product_downloadables_url(@product)}
+    #   end
+    # end
 
-      # create.failure.wants.html do
-      #   render :action => :index
-      # end
+    # create.failure.wants.html do
+    #   render :action => :index
+    # end
 
-      # update.after do
-      #   respond_to do |wants|
-      #     wants.html {redirect_to admin_product_downloadables_url(@product)}
-      #   end
-      # end
+    # update.after do
+    #   respond_to do |wants|
+    #     wants.html {redirect_to admin_product_downloadables_url(@product)}
+    #   end
+    # end
 
-      # create.before do
-      #   if params[:downloadable].has_key? :viewable_id
-      #     if params[:downloadable][:viewable_id] == "All"
-      #       object.viewable_type = 'Product'
-      #       object.viewable_id = @product.id
-      #     else
-      #       object.viewable_type = 'Variant'
-      #       object.viewable_id = params[:downloadable][:viewable_id]
-      #     end
-      #   else
-      #     object.viewable_type = 'Product'
-      #     object.viewable_id = @product.id
-      #   end
-      # end
+    # create.before do
+    #   if params[:downloadable].has_key? :viewable_id
+    #     if params[:downloadable][:viewable_id] == "All"
+    #       object.viewable_type = 'Product'
+    #       object.viewable_id = @product.id
+    #     else
+    #       object.viewable_type = 'Variant'
+    #       object.viewable_id = params[:downloadable][:viewable_id]
+    #     end
+    #   else
+    #     object.viewable_type = 'Product'
+    #     object.viewable_id = @product.id
+    #   end
+    # end
 
-      # destroy.before do
-      #   @viewable = object.viewable
-      # end
+    # destroy.before do
+    #   @viewable = object.viewable
+    # end
 
-      # destroyafter do
-      #   respond_to do |wants|
-      #     wants.html do
-      #       render :text => ""
-      #     end
-      #   end
-      # end
+    # destroyafter do
+    #   respond_to do |wants|
+    #     wants.html do
+    #       render :text => ""
+    #     end
+    #   end
+    # end
 
-      private
+    private
 
-        def load_data
-          @product = Spree::Product.find_by_permalink(params[:product_id])
-          @variants = @product.variants.collect do |variant|
-            [variant.options_text, variant.id ]
-          end
-
-          @variants.insert(0, "All")
-
-          # @download_limits = @product.variants.collect do |variant|
-          #   variant.downloadables.empty? ? ("\"#{variant.id}\": \'\'") : ("\"#{variant.id}\": #{variant.downloadables.first.download_limit}")
-          # end
-
+      def load_data
+        @product = Spree::Product.find_by_permalink(params[:product_id])
+        @variants = @product.variants.collect do |variant|
+          [variant.options_text, variant.id ]
         end
 
-        def set_viewable
-          if params[:downloadable].has_key? :viewable_id
-            if params[:downloadable][:viewable_id] == "All"
-              object.viewable_type = 'Product'
-              object.viewable_id = @product.id
-            else
-              object.viewable_type = 'Variant'
-              object.viewable_id = params[:downloadable][:viewable_id]
-            end
-          else
+        @variants.insert(0, "All")
+
+        # @download_limits = @product.variants.collect do |variant|
+        #   variant.downloadables.empty? ? ("\"#{variant.id}\": \'\'") : ("\"#{variant.id}\": #{variant.downloadables.first.download_limit}")
+        # end
+
+      end
+
+      def set_viewable
+        if params[:downloadable].has_key? :viewable_id
+          if params[:downloadable][:viewable_id] == "All"
             object.viewable_type = 'Product'
             object.viewable_id = @product.id
+          else
+            object.viewable_type = 'Variant'
+            object.viewable_id = params[:downloadable][:viewable_id]
           end
+        else
+          object.viewable_type = 'Product'
+          object.viewable_id = @product.id
         end
-    end
+      end
   end
+
 end
