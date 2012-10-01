6bba5f97370bfec5bccc0084bc1ad2a743cc414c
Mark Villacampa <markvjal@gmail.com>
2012-10-01 11:22:56 +0200

    change tab position

--- app/overrides/add_admin_tab_decorator.rb
+++ app/overrides/add_admin_tab_decorator.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/admin/shared/_product_tabs",
                      :name => "attachments_download_tab",
-                     :insert_after => "[data-hook='admin_product_tabs'], #admin_product_tabs[data-hook]",
+                     :insert_bottom => "[data-hook='admin_product_tabs'], #admin_product_tabs[data-hook]",
                      :partial => "spree/admin/shared/product_downloadables_tab",
                      :disabled => false)
