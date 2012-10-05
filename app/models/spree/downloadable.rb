b5cd76ae0a0a3258fda57ef2241959600801caa3
Mark Villacampa <markvjal@gmail.com>
2012-10-05 03:10:12 +0200

    .

--- app/models/spree/downloadable.rb
+++ app/models/spree/downloadable.rb
@@ -2,8 +2,8 @@ require 'zip/zip'
 
 class Spree::Downloadable < Spree::ProductDownload
   has_attached_file :attachment,
-                    :url => "/downloadable/:id/:basename.:extension",
-                    :path => ":rails_root/public/downloadable/:id/:basename.:extension"
+                    :url => "/assets/downloadable/:id/:basename.:extension",
+                    :path => ":rails_root/public/assets/downloadable/:id/:basename.:extension"
 
   validates_attachment_presence :attachment
 
